c9723f0c7e9acc5a320221a516789484b23b78ea
Leaaep <github.scurvy309@passmail.net>
2026-01-31 19:36:42 +0100

    feat: unit fighting timout

--- unit/unit.gd
+++ unit/unit.gd
@@ -1,8 +1,6 @@
 class_name Unit
 extends CharacterBody2D
 
-signal attack(damage: float)
-
 @export var health: float = 100.0
 @export var damage: float = 10.0
 @export var speed: float = 100.0
@@ -15,6 +13,7 @@ var target: Vector2
 var is_fighting: bool = false
 var last_attacked_enemy: Unit
 var last_attacked_building: Building
+var battle_timout: float = 0.5
 
 var overlappingUnits: Dictionary[Unit, bool] = {} # Hashset
 var overlappingBuildings: Dictionary[Building, bool] = {} # Hashset
@@ -33,7 +32,7 @@ func _process(_delta: float) -> void:
 	elif has_buildings_in_range() and not is_fighting:
 		attack_building()
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if has_buildings_in_range() or has_enemies_in_range():
 		velocity = Vector2.ZERO
 	else:
@@ -65,8 +64,10 @@ func attack_unit() -> void:
 	var enemies_in_range := get_enemies_in_range()
 	if last_attacked_enemy == null:
 		last_attacked_enemy = enemies_in_range.pick_random()
+	await get_tree().create_timer(battle_timout).timeout
 	var random_damage_modifier := randf_range(1.0, 5.0)
-	last_attacked_enemy.hurt(damage + random_damage_modifier)
+	if is_instance_valid(last_attacked_building):
+		last_attacked_enemy.hurt(damage + random_damage_modifier)
 	is_fighting = false
 
 func attack_building() -> void:
@@ -74,8 +75,10 @@ func attack_building() -> void:
 	var enemies_in_range := get_buildings_in_range()
 	if last_attacked_building == null:
 		last_attacked_building = enemies_in_range.pick_random()
+	await get_tree().create_timer(battle_timout).timeout
 	var random_damage_modifier := randf_range(1.0, 5.0)
-	last_attacked_building.hurt(damage + random_damage_modifier)
+	if is_instance_valid(last_attacked_building):
+		last_attacked_building.hurt(damage + random_damage_modifier)
 	is_fighting = false
 
 func get_enemies_in_range() -> Array[Unit]:
