4c1ce67788fe5491a1e306e9cb3e1cfe58bcf0c8
Simon Hischier <simon_hischier@hotmail.com>
2026-02-01 15:05:37 +0100

    world spawning fixed

--- world_nils.gd
+++ world_nils.gd
@@ -17,6 +17,7 @@ static var this: World
 @export var ground_size: Vector2i = Vector2i(30, 50)
 
 var untilCivRespawn: float = 0
+var tile_size := 32
 
 var civilizations: Dictionary[Constants.Civilization, Civilization] = {}
 
@@ -40,8 +41,11 @@ func init_civilizations():
 		
 func spawnCivilization(faction: Constants.Civilization):
 	var civ = civScenes[faction].instantiate() as Civilization
-	#print_debug(get_viewport_rect().size, get_viewport_rect().grow(-100).size)
-	civ.position = Vector2(randf() * get_viewport_rect().grow(-100).size.x, randf() * get_viewport_rect().grow(-50).size.y)
+	var max_width := ground_size.x * tile_size * 0.8
+	var max_height := ground_size.y * tile_size * 0.8
+	
+	var origin_offset := Vector2(ground_size.x * tile_size / 2.0, ground_size.y * tile_size / 2.0)
+	civ.position = Vector2((randf() - 0.5) * max_width + origin_offset.x, (randf() - 0.5) * max_height + origin_offset.y)
 	civ.faction = faction
 	add_child(civ)
 	civilizations[faction] = civ
